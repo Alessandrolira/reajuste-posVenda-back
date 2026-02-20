630f0df1e3a41ebb56e1181ed5f8dac2237eb91d
Alessandro Lira <alessandrolira09@gmail.com>
2026-02-20 12:44:58 -0300

    primeiro commit, aplicação em pé, com o primeiro endpoint

---
+++ src/main/java/reajuste/reajuste_back/ReajusteBackApplication.java
@@ -0,0 +1,13 @@
+package reajuste.reajuste_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReajusteBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReajusteBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/reajuste/reajuste_back/ReajusteBackApplicationTests.java
@@ -0,0 +1,13 @@
+package reajuste.reajuste_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReajusteBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
